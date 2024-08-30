4c28a76a9e69fb69e646069c2ee2aadf8b065abf
hab3rmas <gbelyj002@gmail.com>
2024-08-30 20:29:38 +0300

    Test

--- examples/vpn/src/main.ts
+++ examples/vpn/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@tok/generation';
 
-import App from './App.vue';
+import App from '../../../app/src/App.vue';
 import { default as config } from './config';
 
 bootstrap(App, config);
